993864d69fcbddb6eed47efe0e9229f51ffd0756
Mônica Marçal <monicagmarcal@outlook.com>
2023-02-16 20:58:44 -0300

    feat
    model Reservas

---
+++ HotelJavaAlura/Hotel/src/model/Reservas.java
@@ -0,0 +1,50 @@
+package model;
+
+import java.sql.Date;
+
+public class Reservas {
+    private Integer id;
+    private Date dataEntrada;
+    private Date dataSaida;
+    private String valor;
+    private String formaPagamento;
+
+    public Reservas(Integer id, Date dataEntrada, Date dataSaida, String valor, String formaPagamento) {
+        this.id = id;
+        this.dataEntrada = dataEntrada;
+        this.dataSaida = dataSaida;
+        this.valor = valor;
+        this.formaPagamento = formaPagamento;
+    }
+
+    public Reservas(Date dataEntrada, Date dataSaida, String valor, String formaPagamento) {
+        this.dataEntrada = dataEntrada;
+        this.dataSaida = dataSaida;
+        this.valor = valor;
+        this.formaPagamento = formaPagamento;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getDataEntrada() {
+        return dataEntrada;
+    }
+
+    public Date getDataSaida() {
+        return dataSaida;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public String getFormaPagamento() {
+        return formaPagamento;
+    }
+}
